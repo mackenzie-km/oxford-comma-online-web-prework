249371ef4439c6b0af701222b49e1282deac59dd
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-09 23:26:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ def oxford_comma(array)
 if array.length == 1 
   array.join("")
 elsif array.length == 2 
-  array.last = " and "array.last
   array.join(" and ")
 else
-array.join(", ")
+  array.last += (" and " + array.last)
+  array.join(", ")
 end
 end
