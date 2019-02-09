b8576215008a2867987a4cf1dc4894cf43473f10
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-09 23:28:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+if array.length == 1 
+  array.join("")
+elsif array.length == 2 
+  array.join(" and ")
+else
+array_new_ending = ("and " + array.pop)
+array.push(array_new_ending)
+array.join(", ")
+end
 end
